08f823af6140b5cbe72674ffd93cc7a5d619759c
monocsp <55018831+monocsp@users.noreply.github.com>
2023-03-23 16:47:22 +0900

    [temp][update] plugin

--- ble_advertise/lib/tecsen_plugin.dart
+++ ble_advertise/lib/tecsen_plugin.dart
@@ -1,8 +1,29 @@
-
 import 'tecsen_plugin_platform_interface.dart';
 
 class TecsenPlugin {
   Future<String?> getPlatformVersion() {
     return TecsenPluginPlatform.instance.getPlatformVersion();
   }
+}
+
+class TecsenLibrary {
+  static final tecsenLibrary = Platform.isAndroid
+      ? TecsenAndroidPlugin(const MethodChannel('tecsen_plugin_android'))
+      : Platform.isIOS
+          ? TecsenIoSPlugin(const MethodChannel('tecsen_plugin_ios'))
+          : throw ("TECSEN PLATFORM EXCEPTION");
+
+  static Future startAdvertise(
+          {required String bluetoothSetName,
+          required String uuid,
+          TecsenTXPower? txPower,
+          int? timeOutMiliSec,
+          TecsenAdvertiseMode? advertiseMode}) =>
+      tecsenLibrary.startAdvertise(
+          bluetoothSetName: bluetoothSetName,
+          uuid: uuid,
+          txPower: txPower,
+          timeOutMiliSec: timeOutMiliSec,
+          advertiseMode: advertiseMode);
+  static Future openBlePage() => tecsenLibrary.openBleSettingPage();
 }
