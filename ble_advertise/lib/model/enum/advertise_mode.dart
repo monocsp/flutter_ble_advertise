6806eb26930307b35714b2bc4e8a26e326776b08
monocsp <55018831+monocsp@users.noreply.github.com>
2023-03-23 16:15:05 +0900

    [create] enum AdvertiseMode

---
+++ ble_advertise/lib/model/enum/advertise_mode.dart
@@ -0,0 +1,34 @@
+/// Android Only
+///
+/// The state of Ble AdvertiseMode
+///
+/// The usual flow of state is as follows :
+///
+/// 1. [lowPower], This mode is intended for use cases where power consumption is a critical factor.
+/// The advertising interval is set to 1.28 seconds, which is the longest possible interval, and the advertising power is set to the lowest possible value.
+/// This mode is suitable for devices that need to advertise frequently, but do not require a fast connection.
+///
+/// 2. [balanced], This mode is intended for use cases where a balance between power consumption and advertising frequency is required.
+/// The advertising interval is set to 0.5 seconds, and the advertising power is set to a medium value.
+/// This mode is suitable for devices that need to advertise frequently and require a reasonably fast connection.
+///
+/// 3. [lowLatency], This mode is intended for use cases where a fast connection is required, and power consumption is less critical.
+/// The advertising interval is set to the shortest possible interval, which is 0.1 seconds, and the advertising power is set to the highest possible value.
+/// This mode is suitable for devices that require a fast connection and do not need to advertise frequently.
+
+enum AdvertiseMode {
+  ///Perform Bluetooth LE advertising in low power mode.
+  ///
+  ///This is the default and preferred advertising mode as it consumes the least power.
+  lowPower(0),
+
+  ///Perform Bluetooth LE advertising in balanced power mode.
+  balanced(1),
+
+  ///Perform Bluetooth LE advertising in low latency, high power mode.
+  lowLatency(2);
+
+  final int toInteger;
+
+  const AdvertiseMode(this.toInteger);
+}
